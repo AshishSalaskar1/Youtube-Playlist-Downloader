da545474898741b9892901a174c4b9d9e2772533
Ashish Salaskar <ashishsalaskar1@gmail.com>
2019-03-26 22:20:14 +0530

    v2

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
 const fetch = require('node-fetch')
 const ytParser = require('js-video-url-parser');
+const fs = require('fs');
+const horizon = require('horizon-youtube-mp3');
+const path = require('path');
+// let express = require("express");
+// let port = process.env.PORT || 3000;
 
 let url = 'https://www.youtube.com/playlist?list=PL7416bv7eAxw4UgYX_8ifKMp9KRM4IWaN';
 
 let key = 'AIzaSyAm-PDWgEFDy5c-LoMk5ubQSBR1FbzVT-8';
 
+//express Init
+// var app = express();
+// const publicPath = path.join(__dirname, '/public');
+// app.use(express.static(publicPath));
+
 //get the Playlist ID
 let temp1 = url.split("=");
 const pid = temp1[temp1.length-1];
-// console.log('pid:', pid)
-
-//async fetch call
-let  fetchAsync = async () => { 
-    let res = await fetch(url);
-    let data = await res.json();
-    console.log(data);
-};
-
-//Call YT 3 api
+//Call Youtube 3 api http:/....pid+key
 const finalPlayUrl = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId='+pid+'&key='+key;
 
 let playVidArr;
 let lst = [];
 
-//Make url from Video ID
+//Make url from Video ID using ytParser
 let urlMaker = (urlN) => {
     let urlNew = ytParser.create({
         videoInfo: {
@@ -35,7 +36,8 @@ let urlMaker = (urlN) => {
       return urlNew
 };
 
-fetch(finalPlayUrl)
+let getVideos = (finalPlayUrl)=> {
+  fetch(finalPlayUrl)
     .then(res => res.json())
     .then((out) => {
         console.log("No of Videos",out.items.length);
@@ -45,12 +47,50 @@ fetch(finalPlayUrl)
                     url : urlMaker(ele.snippet.resourceId.videoId)}
         });
 
-
         console.log(lst);
+
+        // lst.forEach(element => {
+        //   downloadVideo(element.url,element.name);         
+        // });
+
+        downloadVideo(lst[0].url,lst[0].name)
+
+        // process.exit()
         
 }).catch(err => console.error(err));
+}
 
-// console.log(lst);
+getVideos(finalPlayUrl);
 
+let downloadVideo = (url,name) => {
 
-  
+  //remove special chards
+  let vidName = name.replace(/[^\w\s]/gi, '_')+".mp3";
+
+  var downloadPath = path.join(__dirname);
+ 
+horizon.downloadToLocal(
+  'https://www.youtube.com/watch?v=Y4fodpIwal8',
+  __dirname+"/mp3",
+  vidName,
+  null,
+  null,
+  onConvertVideoComplete,
+  onConvertVideoProgress
+);
+ 
+function onConvertVideoComplete(err, result) {
+  console.log(err, result);
+}
+ 
+function onConvertVideoProgress(percent, timemark, targetSize) {
+  console.log('Progress:', percent, 'Timemark:', timemark, 'Target Size:', targetSize);
+
+}
+}
+
+console.log("HIIII");
+
+// app.listen(port,() => {
+//   console.log(`Server running on port ${port}`);
+// });
