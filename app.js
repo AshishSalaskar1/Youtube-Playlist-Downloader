d7c6f2e7898a3cec28917e7efc8954635df90eb6
Ashish Salaskar <ashishsalaskar1@gmail.com>
2019-03-28 00:11:25 +0530

    Implemented POST API to get videos

--- app.js
+++ app.js
@@ -1,67 +1,110 @@
 const fetch = require('node-fetch')
 const ytParser = require('js-video-url-parser');
+const bodyParser = require('body-parser');
 const fs = require('fs');
 const horizon = require('horizon-youtube-mp3');
 const path = require('path');
-// let express = require("express");
-// let port = process.env.PORT || 3000;
+let express = require("express");
+let port = process.env.PORT || 3000;
+const _ = require('lodash');
 
-let url = 'https://www.youtube.com/playlist?list=PL7416bv7eAxw4UgYX_8ifKMp9KRM4IWaN';
+// let url;
+let key = 'AIzaSyAm-PDWgEFDy5c-LoMk5ubQSBR1FbzVT-8';
 
-let key = 'ur api key';
+var app = express();
+const publicPath = path.join(__dirname, '/public');
+app.use(express.static(publicPath));
+app.use(bodyParser.json());
 
-//express Init
-// var app = express();
-// const publicPath = path.join(__dirname, '/public');
-// app.use(express.static(publicPath));
-
-//get the Playlist ID
-let temp1 = url.split("=");
-const pid = temp1[temp1.length-1];
-//Call Youtube 3 api http:/....pid+key
-const finalPlayUrl = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId='+pid+'&key='+key;
-
-let playVidArr;
-let lst = [];
-
-//Make url from Video ID using ytParser
+//makes curl from video ID
 let urlMaker = (urlN) => {
-    let urlNew = ytParser.create({
-        videoInfo: {
-          provider: 'youtube',
-          id: urlN,
-          mediaType: 'video'
-        }
-      });
-      return urlNew
+  let urlNew = ytParser.create({
+      videoInfo: {
+        provider: 'youtube',
+        id: urlN,
+        mediaType: 'video'
+      }
+    });
+
+    return urlNew;
 };
 
-let getVideos = (finalPlayUrl)=> {
-  fetch(finalPlayUrl)
-    .then(res => res.json())
-    .then((out) => {
-        console.log("No of Videos",out.items.length);
-        playVidArr = out.items;
-        lst = playVidArr.map((ele) => {
-            return {name : ele.snippet.title,
-                    url : urlMaker(ele.snippet.resourceId.videoId)}
-        });
+//download URL
+// let getVideos = (finalPlayUrl) => {
+//   let lst = [];
+//   let playVidArr
+//   fetch(finalPlayUrl)
+//     .then(res => res.json())
+//     .then((out) => {
+//         console.log("No of Videos",out.items.length);
+//         playVidArr = out.items;
+//         lst = playVidArr.map((ele) => {
+//             return {name : ele.snippet.title,
+//                     url : urlMaker(ele.snippet.resourceId.videoId)}
+//         });
 
-        console.log(lst);
-
-        // lst.forEach(element => {
-        //   downloadVideo(element.url,element.name);         
-        // });
-
-        downloadVideo(lst[0].url,lst[0].name)
-
-        // process.exit()
+//         console.log(lst);
+//         console.log("Returning");
         
-}).catch(err => console.error(err));
-}
 
-getVideos(finalPlayUrl);
+//         // lst.forEach(element => {
+//         //   downloadVideo(element.url,element.name);         
+//         // });
 
+//         // downloadVideo(lst[0].url,lst[0].name)
+
+//         // process.exit()
+        
+// }).catch(err => console.error(err));
+
+// }
+
+let getVideos1 = async (finalPlayUrl) => {
+  let lst = [];
+  let playVidArr
+  const res = await fetch(finalPlayUrl);
+  const out = await res.json();
+
+          console.log("No of Videos",out.items.length);
+          playVidArr = out.items;
+          lst = playVidArr.map((ele) => {
+              return {name : ele.snippet.title,
+                      url : urlMaker(ele.snippet.resourceId.videoId)}
+          });
+
+          console.log(lst);
+          console.log("Returning");
+          return lst;
+
+};
+
+let playlistToVids = async (URL) => {
+    //get the Playlist ID
+    //  url = URL;
+    let temp1 = URL.split("=");
+    const pid = temp1[temp1.length-1];
+
+    //Call Youtube 3 api http:/....pid+key
+    const finalPlayUrl = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId='+pid+'&key='+key;
+
+    console.log(finalPlayUrl);
+    let res = await getVideos1(finalPlayUrl);
+  
+    return res;
+};
+
+
+app.post('/url',(req,res) => {
+  // let VidUrl = res.params.url;
+  let VidUrl = _.pick(req.body,['url']).url;
+
+  playlistToVids(VidUrl)
+    .then((data) => res.send(data))
+    .catch((e) => console.log(e));
+
+});
+
+//download video
 let downloadVideo = (url,name) => {
 
   //remove special chards
@@ -91,6 +134,6 @@ function onConvertVideoProgress(percent, timemark, targetSize) {
 
 console.log("HIIII");
 
-// app.listen(port,() => {
-//   console.log(`Server running on port ${port}`);
-// });
+app.listen(port,() => {
+  console.log(`Server running on port ${port}`);
+});
